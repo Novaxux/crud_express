ebf12ae028c68fb1d56f72f4575640a074b47db7
Novaxux <manuelova@outlook.es>
2025-04-01 20:07:26 -0600

    post api

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+let productos = []
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');  
 })
-  
+
+app.post('/data', (req, res) => {
+  const nuevoProducto = req.body;
+  productos.push(nuevoProducto)
+  console.log(req.body.title);
+  console.log(productos)
+  res.json({msg:'agregado'}); 
+})
+
 
 app.listen(3002, ()=>console.log("Escuchando en 3002: http://localhost:3002/") );
